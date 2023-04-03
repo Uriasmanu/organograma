944acf5698ad79b99394f03472526ef3f79e25f5
Manoela Urias <maanoelaurias@gmail.com>
2023-04-02 22:06:50 -0300

    atualizações

---
+++ src/Componentes/Banner/Banner.js
@@ -0,0 +1,9 @@
+//JSX como o react le para transformar em elemento DOM
+
+function Banner() {
+    return(
+        <img src ="/imagens/banner.png" alt="O banner pricipal da pagina"/>
+    )
+}
+
+export default Banner
